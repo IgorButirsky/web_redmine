ed3933d6b1ab29a43fdc766be6f558d93280478d
Igor Butirsky <butirsky@mlsdev.com>
2014-02-20 15:58:30 +0200

    add ability to comment issue

--- www/js/controllers/issue.js
+++ www/js/controllers/issue.js
@@ -1,8 +1,20 @@
-angular.module("redmineApp").controller("issueCtrl", ["$scope", "$routeParams", "$http", function($scope, $routeParams, $http) {
+angular.module("redmineApp").controller("issueCtrl", ["$scope", "$routeParams", "$http", "$log", function($scope, $routeParams, $http, $log) {
 	$scope.issueId = $routeParams.issueId;
+	$scope.commentText = null;
 
 	$http.get("http://crm.mlsdev.com/issues/" + $scope.issueId + ".json", {params:{key:localStorage.getItem("api_key")}})
 				.success(function(data) {
 					$scope.issue = data.issue;
 				});
+
+	$scope.onCommentClicked = function () {
+		$log.log("onCommentClicked");
+		if ($scope.commentText == null || $scope.commentText.length == 0) {
+			$log.log("comment is empty");
+			return;
+		}
+		$log.log("send comment");
+		var data = {issue:{notes:$scope.commentText}};
+		$http.put("http://crm.mlsdev.com/issues/17408.json", data, {params:{key:localStorage.getItem("api_key")}});
+	}
 }]);
